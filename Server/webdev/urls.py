247c5237ff6cd997d711ca387eca792c81b4e94e
Lander <lachicaizaro@uide.edu.ec>
2025-08-18 00:38:11 -0500

    rutas

--- Server/webdev/urls.py
+++ Server/webdev/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path
 from django.http import HttpResponse
 from django.db import connections
 from django.db.utils import OperationalError
@@ -32,6 +32,6 @@ def health_check(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('detection.urls')),
-    url(r'^api/', include(('alertuploadREST.urls', 'alertuploadREST'), namespace='api')),
-    path('health/', health_check, name='health_check'),  # Añade esta línea
+    re_path(r'^api/', include(('alertuploadREST.urls', 'alertuploadREST'), namespace='api')),
+    path('health/', health_check, name='health_check'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
