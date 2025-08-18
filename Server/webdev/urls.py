e5f1f18377abfea83549f949264139a0022accbf
Lander <lachicaizaro@uide.edu.ec>
2025-08-18 00:51:36 -0500

    new version- urls

--- Server/webdev/urls.py
+++ Server/webdev/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('detection.urls')),
-    url(r'^api/', include(('alertuploadREST.urls', 'alertuploadREST'), namespace='api')),
+    re_path(r'^api/', include(('alertuploadREST.urls', 'alertuploadREST'), namespace='api')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
