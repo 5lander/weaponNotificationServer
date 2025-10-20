30dfcdc23b6a638ac9763a43d0a283e7f56918ed
Lander <lachicaizaro@uide.edu.ec>
2025-10-19 21:07:30 -0500

    connection

--- Server/webdev/settings.py
+++ Server/webdev/settings.py
@@ -39,7 +39,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['weapondetectionsystem.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['weaponnotificationserver.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
