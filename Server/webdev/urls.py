be5aa0b80306c3fff2c78b7fd966fd2da3f7d410
Lander <lachicaizaro@uide.edu.ec>
2025-10-19 20:58:17 -0500

    rutas cambiadas

--- Server/webdev/urls.py
+++ Server/webdev/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path, include, path
-from rest_framework import routers
-from . import views
-from rest_framework.authtoken import views as rest_framework_views
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    # Alert POST
-    path('images/', views.postAlert, name='postalert'),
-    re_path(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
-]
+    path('admin/', admin.site.urls),
+    path('', include('detection.urls')),
+    re_path(r'^api/', include(('alertuploadREST.urls', 'alertuploadREST'), namespace='api')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
